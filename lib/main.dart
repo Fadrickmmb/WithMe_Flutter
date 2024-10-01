af8f506a83a4492e9d348d7c1a235ccaae281fe8
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-10-01 08:57:18 -0400

    Minor UI changes

--- lib/main.dart
+++ lib/main.dart
@@ -38,19 +38,15 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
+
+      body: Column(
         children: [
           ElevatedButton(onPressed: (){
             Navigator.push(context,
               MaterialPageRoute(builder: (context) => UserProfilePage(),),);
           }, child: Text('Profile'),),
-      ],
-    ),
+        ],
+
       ),
     );
   }
