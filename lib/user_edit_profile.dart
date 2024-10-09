7c142aee8178705bb036e1cbd28ac549a58776da
sravani <b.suryasravani@gmail.com>
2024-10-09 19:22:22 -0400

    changes in edit profile added change password and profile photo

--- lib/user_edit_profile.dart
+++ lib/user_edit_profile.dart
@@ -1,6 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:image_picker/image_picker.dart';
 import 'package:withme_flutter/auth_login.dart';
 
 class UserEditProfile extends StatefulWidget {
@@ -11,14 +15,18 @@ class UserEditProfile extends StatefulWidget {
 class _EditProfilePageState extends State<UserEditProfile> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _bioController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
   String userAvatar = '';
   String userId = '';
+  File? _imageFile;
+  final ImagePicker _picker = ImagePicker();
 
   @override
   void initState() {
     super.initState();
     _fetchUserData();
   }
+
 
   Future<void> _fetchUserData() async {
     final FirebaseAuth auth = FirebaseAuth.instance;
@@ -40,21 +48,66 @@ class _EditProfilePageState extends State<UserEditProfile> {
     }
   }
 
+
+  Future<void> _pickImage() async {
+    final pickedFile = await _picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+
+  Future<String?> _uploadImage(File imageFile) async {
+    try {
+      final storageRef = FirebaseStorage.instance.ref().child('user_profile_pictures/$userId.jpg');
+      await storageRef.putFile(imageFile);
+      return await storageRef.getDownloadURL();
+    } catch (e) {
+      print('Error uploading image: $e');
+      return null;
+    }
+  }
+
+
   Future<void> _updateUserProfile() async {
     final FirebaseAuth auth = FirebaseAuth.instance;
     final User? user = auth.currentUser;
 
     if (user != null) {
+      String? imageUrl;
+      if (_imageFile != null) {
+        imageUrl = await _uploadImage(_imageFile!);
+      }
+
       final DatabaseReference userRef = FirebaseDatabase.instance.ref().child('users/${user.uid}');
       await userRef.update({
         'name': _nameController.text,
         'userBio': _bioController.text,
-        'userPhotoUrl': userAvatar, // Update avatar if needed
+        'userPhotoUrl': imageUrl ?? userAvatar,
       });
 
       ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text("Profile updated successfully!")));
-      Navigator.pop(context);
     }
+
+
+    if (_passwordController.text.isNotEmpty) {
+      try {
+        await user!.updatePassword(_passwordController.text.trim());
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Password updated successfully!')),
+        );
+      } catch (error) {
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text('Error updating password: $error')),
+        );
+      }
+    }
+
+
+    Navigator.pop(context);
   }
 
   Future<void> _logoutUser() async {
@@ -62,7 +115,7 @@ class _EditProfilePageState extends State<UserEditProfile> {
     Navigator.pushAndRemoveUntil(
       context,
       MaterialPageRoute(builder: (context) => AuthLogin()),
-          (route) => false, // This removes all the previous routes, ensuring the user can't go back
+          (route) => false,
     );
   }
 
@@ -72,23 +125,24 @@ class _EditProfilePageState extends State<UserEditProfile> {
       appBar: AppBar(
         title: Text('Edit Profile'),
         backgroundColor: Colors.grey,
-        // actions: [
-        //   IconButton(
-        //     icon: Icon(Icons.logout),
-        //     onPressed: _logoutUser, // Trigger the logout function
-        //   ),
-        // ],
       ),
       body: Padding(
         padding: EdgeInsets.all(16.0),
         child: Column(
           children: [
+
             CircleAvatar(
               backgroundColor: Colors.grey,
               radius: 50,
-              backgroundImage: userAvatar.isNotEmpty
+              backgroundImage: _imageFile != null
+                  ? FileImage(_imageFile!)
+                  : userAvatar.isNotEmpty
                   ? NetworkImage(userAvatar)
                   : AssetImage('assets/small_logo.png') as ImageProvider,
+            ),
+            TextButton(
+              onPressed: _pickImage,
+              child: Text('Change Profile Picture'),
             ),
             SizedBox(height: 20),
             TextField(
@@ -105,9 +159,21 @@ class _EditProfilePageState extends State<UserEditProfile> {
                 labelText: "Bio",
                 border: OutlineInputBorder(),
               ),
-              maxLines: 3,
+              maxLines: 1,
             ),
             SizedBox(height: 20),
+
+
+            TextField(
+              controller: _passwordController,
+              decoration: InputDecoration(
+                labelText: "New Password",
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+            ),
+            SizedBox(height: 20),
+
             ElevatedButton(
               onPressed: _updateUserProfile,
               child: Text('Save Changes'),
@@ -133,6 +199,3 @@ class _EditProfilePageState extends State<UserEditProfile> {
     );
   }
 }
-
-
-
