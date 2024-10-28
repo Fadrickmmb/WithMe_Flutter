e0551334d9802340c286d52d2e6eb9deec992a10
Fadrick Barroso <89845529+Fadrickmmb@users.noreply.github.com>
2024-10-28 14:29:20 -0400

    Working on User Suspension

---
+++ lib/admin_add_post_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminAddPostPage extends StatefulWidget {
+  const AdminAddPostPage({super.key});
+
+  @override
+  State<AdminAddPostPage> createState() => _AdminAddPostPageState();
+}
+
+class _AdminAddPostPageState extends State<AdminAddPostPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
